857b60d5dd146bb6548c362939e0400eb4796f72
Jesse Quijano <jesse@quijano.net>
2019-04-06 10:42:59 -0700

    update landing to include Ethika logo and relevant links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="{{ asset('images/ethika.png') }}" alt="Ethika logo" />
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">New Order</a>
+                    <a href="#">Search Orders</a>
+                    <a href="#">LinkedIn</a>
+                    <a href="#">GitHub</a>
+                    <a href="#">Stack Overflow</a>
+                    <a href="#">Exercism.io</a>
                 </div>
             </div>
         </div>
