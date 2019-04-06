0e50857e29f4248a83f87a114706907eed2426d8
Jesse Quijano <jesse@quijano.net>
2019-04-06 09:40:15 -0700

    added routes for order management functions

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* version 1.0 routes - allows for backward compatibility */
+Route::middleware('web')->prefix('1.0')->group(function() {
+    Route::post('/order-save', 'ApiController@saveOrder');
+    Route::patch('/order-update', 'ApiController@updateOrder');
+    Route::put('/order-update', 'ApiController@updateOrder');
+    Route::get('/order/{id}', 'ApiController@getOrder');
+    Route::delete('/order-delete/{id}', 'ApiController@deleteOrder');
+});
