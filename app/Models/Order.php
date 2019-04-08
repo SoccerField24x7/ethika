0ec2266e61770fef58ad5fedac80b230cb98ac7b
Jesse Quijano <jesse@quijano.net>
2019-04-07 20:02:29 -0700

    overload toObject() method to pass in instance of Order object (if nec.)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,16 +1,32 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\ModelWithValidation;
 
-class Order extends Model
+class Order extends ModelWithValidation
 {
     protected $table = 'order';
     public $timestamps = false;
     protected $guarded = ['id'];
 
-    public function order_details()
+    public function order_items()
     {
-        $this->hasMany('App\Models\OrderItem', 'order_id', 'id');
+        return $this->hasMany('App\Models\OrderItem', 'order_id', 'id');
+    }
+
+    public static function toObject(array $data, $obj = null) : Order
+    {
+
+        if ($obj == null) {
+            $obj = new self();
+        }
+
+//        if (isset($vals->order_items)) {
+//            foreach ($vals->order_items as $line) {
+//
+//            }
+//        }
+
+        return parent::toObject($data, $obj);
     }
 }
