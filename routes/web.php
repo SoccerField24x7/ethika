4fd9ffe246b57423bb5c420a68c6bf341ca904ad
Jesse Quijano <jesse@quijano.net>
2019-04-08 15:57:27 -0700

    New web routes for order and search pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/order', function() {
+    return view('order');
+});
+Route::get('/search', function() {
+    return view('search');
+});
