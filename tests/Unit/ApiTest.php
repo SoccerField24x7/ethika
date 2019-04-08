3cb0195b44a3e89b4052124919fe2c1b28a34d36
Jesse Quijano <jesse@quijano.net>
2019-04-08 08:53:41 -0700

    passing test for validation failure

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -37,8 +37,19 @@ class APITest extends TestCase
 
     public function testAddNewOrderFailsValidationNoLastName()
     {
-        $order = '[{"id":1,"first_name":"Test","email":"1@quijano.net","created_at":"2019-04-06 23:04:00","updated_at":null,"order_items":[{"order_id":1,"line_number":1,"name":"undies","quantity":3,"created_at":"2019-04-06 23:08:33","updated_at":null}]}]';
-        $this->markTestSkipped();
+        $order = '{"first_name":"Test","email":"' . uniqid() . '@quijano.net","created_at":"2019-04-06 23:04:00","updated_at":null,"order_items":[{"order_id":1,"line_number":1,"name":"undies","quantity":3,"created_at":"2019-04-06 23:08:33","updated_at":null}]}';
+        $response = $this->call('post',
+            '/api/1.0/order-save', // action (defined in routes.php)
+            [],
+            [],
+            [],
+            $this->headers,
+            $order
+        );
+
+        $result = json_decode($response->getContent());
+
+        $this->assertTrue(isset($result->Error) ? true : false);
     }
 
     public function testGetOrderByIdSuccess()
@@ -53,6 +64,7 @@ class APITest extends TestCase
 
     public function testPutOrder()
     {
+        $order = '{"id":1,"first_name":"Test","last_name":"Order","email":"1@quijano.net","created_at":"2019-04-06 23:04:00","updated_at":null,"order_items":[{"order_id":1,"line_number":1,"name":"undies","quantity":3,"created_at":"2019-04-06 23:08:33","updated_at":null}]}';
         $this->markTestSkipped();
     }
 
