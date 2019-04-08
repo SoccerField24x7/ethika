46762eee6cd4319419ebb4a2350e334dc1c7b4d9
Jesse Quijano <jesse@quijano.net>
2019-04-08 11:16:21 -0700

    wrap save in transaction, implement GET Order

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class ApiController extends BaseController
 {
@@ -20,7 +21,7 @@ class ApiController extends BaseController
         $orderData = $request->input();
 
         if (empty($orderData) || !is_array($orderData)) {
-            return json_encode(['Error' => 'Invalid order data.']);
+            return json_encode(['Error' => 'Invalid Order data.']);
         }
 
         /* attempt to convert to object, then validate */
@@ -37,54 +38,84 @@ class ApiController extends BaseController
             return json_encode(['Error' => $ex->getMessage()]);
         }
 
-        /* in case they are trying to update via POST */
+        /* in case they are trying to update via POST, send them to updateOrder() */
         if (!empty($order->id))
         {
             return $this->updateOrder($request);
         }
 
+        /* wrap in a transaction in case line item saves fail */
+        DB::beginTransaction();
         try {
             $order->save();
 
             /* now handle line items */
             foreach ($order->order_items as $line) {
                 $line->order_id = $order->id; // ensure lines get the new order id we just created
+
                 $line->save();
             }
         } catch (\Exception $ex) {
+            DB::rollBack();
             return json_encode(['Error' => $ex->getMessage()]);
         }
+        DB::commit();
 
         /* cache order in the orders 'folder' for 10 minutes (longer in prod environment) */
         Cache::store('redis')->tags(['orders'])->put($order->id, json_encode($order), 600);
 
-        return json_encode(['Success' => true, 'data' => json_encode($order)]);
+        return json_encode(['Success' => true, 'data' => $order]);
     }
 
     public function updateOrder(Request $request) : string
     {
+        $good = false;
+
         // persist
 
         // expire cache/re-cache
 
-        return '';  // create standardized response object, then return
+        return json_encode(['Error' => 'Method Not Implemented']);  // create standardized response object, then return
     }
 
+    /**
+     * @param int $orderId
+     * @return string
+     */
     public function getOrder(int $orderId) : string
     {
-        // check cache and return
+        if (empty($orderId) || $orderId == 0) {
+            return json_encode(['Error' => 'Invalid Order data.']);
+        }
 
+        /* check cache and return */
+        $order = Cache::store('redis')->tags(['orders'])->get($orderId);
 
-        //
+        /* not in cache, let's load */
+        if (!$order) {
+            $order = Order::find($orderId);
+            if ($order != null) {
+                /* found it, cache it! */
+                Cache::store('redis')->put($orderId, json_encode($order));
+            }
+        }
+
+        if (empty($order)) { // null or false
+            return json_encode(['Error' => 'Invalid Order Id.']);
+        }
+
+        return json_encode(['Success' => true, 'data' => $order]);
     }
 
     public function deleteOrder(int $orderId) : string
     {
-        // remove from db
+        $good = false;
 
-        // expire from cache
+        // persist
 
-        return ''; // create standardized response object, then return
+        // expire cache/re-cache
+
+        return json_encode(['Error' => 'Method Not Implemented']);  // create standardized response object, then return
     }
 
     /**
