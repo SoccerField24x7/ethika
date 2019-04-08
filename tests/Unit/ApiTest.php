527b7a6009ae1ce1886ad2c3ab820253dc5edd3b
Jesse Quijano <jesse@quijano.net>
2019-04-08 08:18:01 -0700

    test for adding new order passing

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -4,19 +4,40 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\Order;
+use Illuminate\Foundation\Testing\TestResponse;
 
 class APITest extends TestCase
 {
     // TODO:  Add PHPDoc comments
 
+    private $headers = [
+        'CONTENT_TYPE' => 'application/json',
+        'HTTP_ACCEPT' => 'application/json',
+        'Cache-control' => 'no-cache',
+    ];
+
     public function testAddNewOrderSuccess()
     {
-        $this->markTestSkipped();
+
+        $order = '{"first_name":"Test","last_name":"Order","email":"' . uniqid() . '@quijano.net","created_at":"' . gmdate('Y-m-d H:i:s') .'","updated_at":null,"order_items":[{"order_id":1,"line_number":1,"name":"undies","quantity":3,"created_at":"2019-04-06 23:08:33","updated_at":null}]}';
+
+        $response = $this->call('post',
+            '/api/1.0/order-save', // action (defined in routes.php)
+            [],
+            [],
+            [],
+            $this->headers,
+            $order
+        );
+
+        $result = json_decode($response->getContent());
+
+        $this->assertTrue(isset($result->Success) ? true : false);
     }
 
-    public function testAddNewOrderFailsValidation()
+    public function testAddNewOrderFailsValidationNoLastName()
     {
+        $order = '[{"id":1,"first_name":"Test","email":"1@quijano.net","created_at":"2019-04-06 23:04:00","updated_at":null,"order_items":[{"order_id":1,"line_number":1,"name":"undies","quantity":3,"created_at":"2019-04-06 23:08:33","updated_at":null}]}]';
         $this->markTestSkipped();
     }
 
@@ -44,4 +65,5 @@ class APITest extends TestCase
     {
         $this->markTestSkipped();
     }
+
 }
