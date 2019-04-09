25b1b0b24d576aaec2f1e562cf1aa9ed9175e9bc
Jesse Quijano <jesse@quijano.net>
2019-04-08 22:53:43 -0700

    autoload last 100 orders into Redis cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
+use App\Models\Order;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /* pre-load redis with last 100 orders (totally arbitrary) */
+        $orders = Order::with('order_items')->orderBy('id', 'DESC')->limit(100)->get();
+        foreach ($orders as $order) {
+            Cache::store('redis')->tags(['orders'])->put($order->id, json_encode($order), 600);
+        }
     }
 }
